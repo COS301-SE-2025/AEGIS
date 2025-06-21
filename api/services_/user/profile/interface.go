b42ff4d9a980c8ff7035df77db75f8fb6ced09a2
Tshireletso (u22636022) <u22636022@tuks.co.za>
2025-06-21 17:41:26 +0200

    implemented profile update

---
+++ api/services_/user/profile/interface.go
@@ -0,0 +1,14 @@
+package profile
+
+// ProfileRepository defines the behavior for working with user profile data.
+type ProfileRepository interface {
+	// GetProfileByID retrieves the profile information of a user by their ID.
+	GetProfileByID(userID string) (*UserProfile, error)
+
+	// UpdateProfile updates the user's name, email, and optionally profile picture.
+	UpdateProfile(data *UpdateProfileRequest) error
+}
+type IPFSUploader interface {
+    UploadProfilePicture(filename string, fileData []byte, userID string) (string, error)
+    DeleteFile(hash string) error
+}
