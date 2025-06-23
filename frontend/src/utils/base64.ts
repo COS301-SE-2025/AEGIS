7f610ff78f43c9509402162e62186bb03137e425
Ofentse360 <u22640020@tuks.co.za>
2025-06-23 14:01:28 +0200

    Frontend: utils functions

---
+++ frontend/src/utils/base64.ts
@@ -0,0 +1,13 @@
+export function encodeBase64(data: Uint8Array): string {
+  return btoa(String.fromCharCode(...data));
+}
+
+export function decodeBase64(base64: string): Uint8Array {
+  const binaryString = atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
