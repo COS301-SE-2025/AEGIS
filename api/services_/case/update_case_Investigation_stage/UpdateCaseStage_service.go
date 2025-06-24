c782d4123413740bd78c1a8bcb571d612556598f
Onthatile-22659812 <onthatilemonox@gmail.com>
2025-06-24 17:03:25 +0200

    evidence totals, tag case,update stage servies

---
+++ api/services_/case/update_case_Investigation_stage/UpdateCaseStage_service.go
@@ -0,0 +1,44 @@
+package update_case_stage
+
+import (
+	"aegis-api/models"
+	"aegis-api/repositories"
+	"errors"
+	 "github.com/google/uuid"
+	 "fmt"
+)
+
+type CaseService interface {
+    UpdateCaseStage(caseID string, newStage models.InvestigationStage) error
+}
+
+type caseService struct {
+    repo repositories.UpdateCaseStageRepository
+}
+
+func NewCaseService(r repositories.UpdateCaseStageRepository) CaseService {
+    return &caseService{repo: r}
+}
+
+//validates the new stage and checks if the case exists before updating 
+func (s *caseService) UpdateCaseStage(caseID string, newStage models.InvestigationStage) error {
+   
+	if !newStage.IsValid() {// Assuming IsValid is a method on InvestigationStage that checks if the stage is valid
+		return errors.New("invalid investigation stage")
+	}
+
+	id, err := uuid.Parse(caseID) //Convert string to uuid.UUID
+    if err != nil {
+        return fmt.Errorf("invalid UUID: %w", err) // Return an error if the caseID is not a valid UUID
+    }
+
+    exists, err := s.repo.CaseExists(id) // Check if the case exists in the db
+    if err != nil {
+        return err // Return an error if there was an issue checking the case existence
+    }
+    if !exists {
+        return errors.New("case not found") // Return an error if the case does not exist
+    }
+
+    return s.repo.UpdateStage(caseID, newStage) // Update the case stage in the db
+}
