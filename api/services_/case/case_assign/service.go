42830b147624098d451f44036bcc7b746c1a9c9d
Tshireletso (u22636022) <u22636022@tuks.co.za>
2025-08-01 11:33:21 +0200

    Services: case_assign and registration

--- api/services_/case/case_assign/service.go
+++ api/services_/case/case_assign/service.go
@@ -18,9 +18,9 @@ func NewCaseAssignmentService(
 // This method now takes the assigner's role directly
 func (s *CaseAssignmentService) AssignUserToCase(
 	assignerRole string,
-	assignerID uuid.UUID,
 	assigneeID uuid.UUID,
 	caseID uuid.UUID,
+	assignerID uuid.UUID,
 	role string,
 	tenantID uuid.UUID, // Pass the tenant ID of the assigner
 ) error {
@@ -28,22 +28,22 @@ func (s *CaseAssignmentService) AssignUserToCase(
 		return errors.New("forbidden: admin privileges required")
 	}
 
-	// Fetch both users
-	assigner, err := s.userRepo.GetUserByID(assignerID)
-	if err != nil {
-		return err
-	}
-	assignee, err := s.userRepo.GetUserByID(assigneeID)
-	if err != nil {
-		return err
-	}
+	// // Fetch both users
+	// assigner, err := s.userRepo.GetUserByID(assignerID)
+	// if err != nil {
+	// 	return err
+	// }
+	// assignee, err := s.userRepo.GetUserByID(assigneeID)
+	// if err != nil {
+	// 	return err
+	// }
 
-	// Ensure both belong to the same tenant
-	if assigner.TenantID != assignee.TenantID {
-		return errors.New("cannot assign users from a different tenant")
-	}
+	// // Ensure both belong to the same tenant
+	// if assigner.TenantID != assignee.TenantID {
+	// 	return errors.New("cannot assign users from a different tenant")
+	// }
 
-	return s.repo.AssignRole(assigneeID, caseID, role)
+	return s.repo.AssignRole(assigneeID, caseID, role, tenantID)
 }
 
 func (s *CaseAssignmentService) UnassignUserFromCase(ctx *gin.Context, assigneeID, caseID uuid.UUID) error {
