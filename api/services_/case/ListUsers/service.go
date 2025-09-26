584e878023354babd0b71636983ad79c16bc1c78
Tshireletso (u22636022) <u22636022@tuks.co.za>
2025-09-27 01:00:31 +0200

    Backend: Retrieve Users per Tenant

--- api/services_/case/ListUsers/service.go
+++ api/services_/case/ListUsers/service.go
@@ -13,8 +13,8 @@ func NewListUserService(repo ListUserRepository) ListUserService {
 func (s *listUserService) ListUsers(ctx context.Context) ([]User, error) {
 	return s.repo.GetAllUsers(ctx)
 }
-func (s *listUserService) ListUsersByTenant(ctx context.Context, tenantID uuid.UUID) ([]User, error) {
-	return s.repo.GetUsersByTenant(ctx, tenantID)
+func (s *listUserService) ListUsersByTenant(ctx context.Context, tenantID uuid.UUID, page int, pageSize int) ([]User, int64, error) {
+	return s.repo.GetUsersByTenant(ctx, tenantID, page, pageSize)
 }
 func (s *listUserService) GetUserByID(ctx context.Context, userID uuid.UUID) (*User, error) {
 	return s.repo.GetUserByID(ctx, userID)
