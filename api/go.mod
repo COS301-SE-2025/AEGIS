b11b3d14c0638fb5c95840ac6fe45b28a2d3613e
Onthatile-22659812 <onthatilemonox@gmail.com>
2025-10-01 14:45:26 +0200

    encryption at rest for user info

--- api/go.mod
+++ api/go.mod
@@ -25,6 +25,7 @@ require (
 
 require (
 	github.com/docker/go-connections v0.6.0
+	github.com/hashicorp/vault/api v1.21.0
 	github.com/jackc/pgconn v1.14.3
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/prometheus/client_golang v1.14.0
@@ -37,12 +38,24 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-jose/go-jose/v4 v4.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.2.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.37.0 // indirect
 	github.com/prometheus/procfs v0.8.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
+	github.com/ryanuber/go-glob v1.0.0 // indirect
+	golang.org/x/time v0.12.0 // indirect
 )
 
 require (
@@ -54,7 +67,7 @@ require (
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
-	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/containerd/errdefs v1.0.0 // indirect
 	github.com/containerd/errdefs/pkg v0.3.0 // indirect
--- api/services_/user/GetUpdate_UserInfo/User_dto.go
+++ api/services_/user/GetUpdate_UserInfo/User_dto.go
@@ -4,6 +4,9 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"aegis-api/pkg/encryption"
+	gorm "gorm.io/gorm"
+	"context"
 )
 
 type User struct {
@@ -26,4 +29,78 @@ type User struct {
 type UserRole struct {
 	UserID uuid.UUID `gorm:"type:uuid;primaryKey"`
 	Role   string    `gorm:"type:user_role;primaryKey"`
+}
+
+
+
+func (u *User) BeforeSave(tx *gorm.DB) error {
+	ctx := context.Background()
+	service := encryption.GetService()
+
+	// Encrypt Email
+	if u.Email != "" && !encryption.IsEncryptedFormat(u.Email) {
+		encrypted, err := encryption.EncryptString(ctx, service, u.Email)
+		if err != nil {
+			return err
+		}
+		u.Email = encrypted
+	}
+
+	// Encrypt PasswordHash
+	if u.PasswordHash != "" && !encryption.IsEncryptedFormat(u.PasswordHash) {
+		encrypted, err := encryption.EncryptString(ctx, service, u.PasswordHash)
+		if err != nil {
+			return err
+		}
+		u.PasswordHash = encrypted
+	}
+
+	// Encrypt ProfilePictureURL
+	if u.ProfilePictureURL != "" && !encryption.IsEncryptedFormat(u.ProfilePictureURL) {
+		encrypted, err := encryption.EncryptString(ctx, service, u.ProfilePictureURL)
+		if err != nil {
+			return err
+		}
+		u.ProfilePictureURL = encrypted
+	}
+
+	return nil
+}
+
+func (u *User) AfterFind(tx *gorm.DB) error {
+	ctx := context.Background()
+	service := encryption.GetService()
+
+	// Decrypt Email
+	if u.Email != "" {
+		decrypted, err := encryption.DecryptString(ctx, service, u.Email)
+		if err != nil {
+			return err
+		}
+		u.Email = decrypted
+	}
+
+	// Decrypt PasswordHash
+	if u.PasswordHash != "" {
+		decrypted, err := encryption.DecryptString(ctx, service, u.PasswordHash)
+		if err != nil {
+			return err
+		}
+		u.PasswordHash = decrypted
+	}
+
+	// Decrypt ProfilePictureURL
+	if u.ProfilePictureURL != "" {
+		decrypted, err := encryption.DecryptString(ctx, service, u.ProfilePictureURL)
+		if err != nil {
+			return err
+		}
+		u.ProfilePictureURL = decrypted
+	}
+
+	return nil
+}
+
+func (u *User) AfterCreate(tx *gorm.DB) error {
+	return u.AfterFind(tx)
 }
